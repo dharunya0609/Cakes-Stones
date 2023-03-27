48abcfbb09914a4f8c04de4422625461ba27b340
harinijayakumar <harinijk28@gmail.com>
2023-03-27 13:28:45 +0530

    hello changes

--- src/Components/home.js
+++ src/Components/home.js
@@ -7,6 +7,7 @@ export default function Home()
    
       <main>
         <section className="hero">
+          <h1>hello</h1>
           <h2>The Signature Cakes and Stones</h2>
           <p>Indulge in our mouth-watering cakes made with the freshest ingredients.</p>
           <button>Order Now</button>
