ba25b979d3546419c61fc56057fcda026918d629
dharunya0609 <dharunyaramesh800@gmail.com>
2023-04-04 08:57:11 +0530

    ap4

--- src/Components/orders.js
+++ src/Components/orders.js
@@ -1,11 +1,9 @@
 import {useNavigate} from "react-router-dom";
 
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+
 import './order.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 export default function Order()
 {
         const navig=useNavigate();
